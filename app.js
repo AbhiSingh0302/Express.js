e5ea8bcae15262a43718045e4f6696cdf181923f
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-03-10 16:18:48 +0530

    express added

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const http = require('http');
 
+const express = require('express');
+
+const app = express();
+
 const server = http.createServer((req,res)=>{
     res.write("<h1>Hello</h1>");
 });
