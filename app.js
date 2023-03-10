b1e742b7f20c378480c00412a2560c4573ea3741
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-03-10 17:41:03 +0530

    parsing different requests

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product',(req,res,next)=>{
     res.send("<form action='/product' method='POST'><input type='text' name='title'><button>Add Product</button></form>")
 })
 
 app.use('/product',(req,res,next)=>{
-    res.send("<h1>Product Page</h1>");
+    console.log(req.body);
+    res.redirect('/');
 })
 
 app.use('/',(req,res,next)=>{
