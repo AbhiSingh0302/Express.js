4ced7d60e5ae33630685a39d0feddf2f60e4ba9d
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-03-10 17:28:58 +0530

    handling different routes

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-
 const express = require('express');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log("Inside middleware 1!!");
-    next();
+
+app.use('/add-product',(req,res,next)=>{
+    res.send("<form action='/product' method='POST'><input type='text' name='title'><button>Add Product</button></form>")
 })
 
-app.use((req,res,next)=>{
-    console.log("Inside middleware 2!!");
+app.use('/product',(req,res,next)=>{
+    res.send("<h1>Product Page</h1>");
+})
+
+app.use('/',(req,res,next)=>{
     res.send("<h1>Hello from nodejs</h1>");
 })
 
