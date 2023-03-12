a75b6afaefaa6d494e88e013160a81a4906356c4
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-03-12 15:47:40 +0530

    simplified path.join

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
+const rootdir = require('../utils/path');
+
 const path = require('path');
 
 const router = express.Router();
 
 router.get('/add-product',(req,res,next)=>{
     // res.send("<form action='/admin/add-product' method='POST'><input type='text' name='title'><input type='number' name='amount'><button>Add Product</button></form>")
-    res.sendFile(path.join(__dirname,"../","views","add-product.html"));
+    // res.sendFile(path.join(__dirname,"../","views","add-product.html"));
+    res.sendFile(path.join(rootdir,"views","add-product.html"));
 })
 
 router.post('/add-product',(req,res,next)=>{
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
+const rootdir = require('../utils/path');
+
 const path = require('path');
 
 const router = express.Router();
 
 router.get('/',(req,res,next)=>{
     // res.send("<h1>Hello from nodejs</h1>");
-    res.sendFile(path.join(__dirname,"../","views","shop.html"));
+    // res.sendFile(path.join(__dirname,"../","views","shop.html"));
+    res.sendFile(path.join(rootdir,"views","shop.html"));
 })
 
 module.exports = router;
